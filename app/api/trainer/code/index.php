25b4a19f41208fd0f627b3a10b11239cdd11d65b
homefit-dev <in20200212@gmail.com>
2021-04-25 16:57:51 +0900

    share pokemon trainer code init

---
+++ app/api/trainer/code/index.php
@@ -0,0 +1,12 @@
+<?php
+
+require '../../vendor/autoload.php';
+$dbHost = 'database-1.cwquh8kas3fc.us-east-2.rds.amazonaws.com';
+$db = \ParagonIE\EasyDB\Factory::fromArray([
+    "mysql:host={$dbHost};dbname=pokemon",
+    'admin',
+    'db210424'
+]);
+
+
+$db->run('INSERT INTO trainer_code SET code =? ', $_POST['trainerCode']);
---
+++ app/assets/js/share-pokemon-trainer-code.js
@@ -0,0 +1,16 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        trainerCode: '',
+    },
+    methods: {
+        clickShare(){
+            axios.post('/api/trainer/code/', {trainerCode: this.trainerCode}).then(()=>{
+                alert('Shared!')
+            }).catch(() => {
+                alert('Error! Retry!')
+                window.location.reload()
+            })
+        }
+    }
+})
