6b1df8f4c9649306c37023e7a811fdc5c0e1fd19
homefit-dev <in20200212@gmail.com>
2021-04-25 17:51:32 +0900

    qa

--- app/api/trainer/codes/index.php
+++ app/api/trainer/codes/index.php
@@ -5,7 +5,7 @@ use Common\DB;
 require '../../required.php';
 $json = getJsonParameter();
 
-$rows = DB::run('SELECT * FROM trainer_code WHERE invalid = 0 ORDER BY id DESC ');
+$rows = DB::run('SELECT * FROM trainer_code WHERE is_invalid = 0 ORDER BY id DESC ');
 
 responseJson([
     'trainerCodes' => $rows
